bc394a8b55c160d0155f1b44ec56b957b268d767
Yakov Novgorodsky <novgorrr@gmail.com>
2023-10-03 11:04:25 +0300

    Project 7 corrected

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-undef
+// Hi Evgeny, not shure if i did the right thing with the api_url variable. If what i did is wrong it would be great if youll provide the instructions
 module.exports = {
     API_URL: "https://67c8e971-6587-4f13-9310-a5c1df7679bd.serverhub.tripleten-services.com"
-}
+};
